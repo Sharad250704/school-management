064a8cf2b12839b34c1c0ae6e76a90e6b5eb5143
Sharad Pandey <146360332+Sharad250704@users.noreply.github.com>
2024-11-22 19:16:11 +0530

    Update schoolroutes.js

--- routes/schoolroutes.js
+++ routes/schoolroutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const addSchool = require('../schoolcontroller/addschoolcontroller');
-const listSchools = require('../schoolcontroller/listschoolcontroller');
+const addSchool = require('../api/addschoolcontroller');
+const listSchools = require('../api/listschoolcontroller');
 router.post('/addSchool', addSchool);
 router.get('/listSchools', listSchools);
 module.exports = router;
